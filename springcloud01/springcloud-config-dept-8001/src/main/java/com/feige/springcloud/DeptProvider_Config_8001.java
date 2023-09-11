d00545ccfde0b66f732bf3b7a5944bfd6bdb16a5
emptyOVO <gaoyifei040229@163.com>
2023-09-11 22:32:07 +0800

    final study program

---
+++ springcloud01/springcloud-config-dept-8001/src/main/java/com/feige/springcloud/DeptProvider_Config_8001.java
@@ -0,0 +1,22 @@
+package com.feige.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 高毅飞
+ */
+//启动类
+
+
+
+@SpringBootApplication
+@EnableEurekaClient//自动在服务启动后自动注册到eureka
+@EnableDiscoveryClient//服务发现
+public class DeptProvider_Config_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_Config_8001.class,args);
+    }
+}
--- springcloud01/springcloud-config-server-3344/src/main/java/com/feige/springcloud/Config_Server_3344.java
+++ springcloud01/springcloud-config-server-3344/src/main/java/com/feige/springcloud/Config_Server_3344.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // 开启spring cloud config server服务
 @SpringBootApplication
-@EnableConfigServer
 public class Config_Server_3344 {
     public static void main(String[] args) {
         SpringApplication.run(Config_Server_3344.class,args);
